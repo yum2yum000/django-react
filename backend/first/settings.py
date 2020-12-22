a7eee6c15ebefc0cecb6aacd91d27e8400d606cd
morteza <khodemousa@gmail.com>
2020-12-22 17:05:08 +0330

    d

--- backend/first/settings.py
+++ backend/first/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'http://localhost:8000'
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'first.urls'
